082346a23c194299e0782db24b7e27da6d8e4ff0
yushinhe <robin124708476@gmail.com>
2021-04-11 19:07:24 +0800

    再試一次

--- js/knowledge.js
+++ js/knowledge.js
@@ -1,12 +1,5 @@
-var flex = document.querySelector('.flex');
-var msnry = new Masonry(flex, {
+var grid = document.querySelector('.flex');
+var msnry = new Masonry(grid, {
     itemSelector: '.grid-item',
 });
-//  button
-let button=document.querySelectorAll('button')
-console.log(button);
-button.forEach(e=>{
-    e.addEventListener('click',()=>{
-        window.location.href ="./article.html"
-    })
-})
+
