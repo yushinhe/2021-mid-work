a999685e87afdaa799315877aaee968612f19977
yushinhe <robin124708476@gmail.com>
2021-04-11 16:10:53 +0800

    '完成article'

--- js/knowledge.js
+++ js/knowledge.js
@@ -2,3 +2,11 @@ var flex = document.querySelector('.flex');
 var msnry = new Masonry(flex, {
     itemSelector: '.grid-item',
 });
+//  button
+let button=document.querySelectorAll('button')
+console.log(button);
+button.forEach(e=>{
+    e.addEventListener('click',()=>{
+        window.location.href ="../article.html"
+    })
+})
